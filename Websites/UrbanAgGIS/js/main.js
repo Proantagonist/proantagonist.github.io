370ae7c66b7295df810d6e4ad144aa71029c67e7
Proantagonist <imacleopard2010@gmail.com>
2014-07-16 17:13:26 -0500

    added css

--- Websites/UrbanAgGIS/js/main.js
+++ Websites/UrbanAgGIS/js/main.js
@@ -1,9 +1,21 @@
-$(document).ready(function(){
-    $('ul.menu ul').slideUp();
-    $('ul.menu>li').click(function(){
-        $(this).next('ul').slideToggle();
-        $(this).children('h4').toggleClass('active');
-//        $(this).siblings('ul.menu>li').slideToggle();
-    });
-    
-})
+//$(document).ready(function(){
+//    $('ul.menu ul').slideUp();
+//    $('ul.menu>li').click(function(){
+//        $(this).next('ul').slideToggle();
+//        $(this).children('h4').toggleClass('active');
+////        $(this).siblings('ul.menu>li').slideToggle();
+//    });
+//    
+//})
+
+            $(document).ready(function() {
+            $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
+            disableOn: 700,
+            type: 'iframe',
+            mainClass: 'mfp-fade',
+            removalDelay: 160,
+            preloader: false,
+
+            fixedContentPos: false
+            });
+            });
