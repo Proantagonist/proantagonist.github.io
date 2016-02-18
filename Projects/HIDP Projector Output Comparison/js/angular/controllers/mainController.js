18617abd74a9a80c215321364f45a7152ae2f5dc
proantagonist <imacleopard2010@gmail.com>
2016-02-18 01:45:31 -0600

    Beta 1.0

--- Projects/HIDP Projector Output Comparison/js/angular/controllers/mainController.js
+++ Projects/HIDP Projector Output Comparison/js/angular/controllers/mainController.js
@@ -1,5 +1,4 @@
 app.controller('mainController', ['$scope', function ($scope) {
-    $scope.step = 1;
 
     $scope.verifyUrl = function (url) {
         var http = new XMLHttpRequest();
@@ -9,27 +8,44 @@ app.controller('mainController', ['$scope', function ($scope) {
     }
 
     $scope.load = function (value, elem) {
-        //                var path = window.location.href + '/img/' + inputValue + '/' + inputValue + '-' + step + '.jpg'; //Server path. Uncomment before uploading.
-        var path = window.location.origin + '/img/' + value + '/' + value + '-' + $scope.step + '.jpg'; //Local path. Comment before uploading
+                        var path = window.location.href + '/img/' + inputValue + '/' + inputValue + '-' + step + '.jpg'; //Server path. Uncomment before uploading.
+//        var path = window.location.origin + '/img/' + value + '/' + value + '-' + $scope.step + '.jpg'; //Local path. Comment before uploading
 
-        if ($scope.verifyUrl(path)) {
-            (angular.element(event.target)).parent().find('.image-container').empty().css('background-image', 'url(' + path + ')');
+        if (elem) {
+            if ($scope.verifyUrl(path)) {
+                $(elem).parent().find('.image-container').empty().css('background-image', 'url(' + path + ')');
+            } else {
+                $(elem).parent().find('.image-container').css('background-image', 'none').html("<p>There's nothing here Jim.</p>");
+            }
         } else {
-            (angular.element(event.target)).parent().find('.image-container').html("<p>There's nothing here Jim.</p>");
+            if ($scope.verifyUrl(path)) {
+                (angular.element(event.target)).parent().find('.image-container').empty().css('background-image', 'url(' + path + ')');
+            } else {
+                (angular.element(event.target)).parent().find('.image-container').css('background-image', 'none').html("<p>There's nothing here Jim.</p>");
+            }
         }
     };
 
-    function controls() {
-        $('#manual-exposure i').click(function () {
-            if ($(this).attr('id') === 'plus') {
-                step = step + 1;
-            } else if ($(this).attr('id') === 'minus') {
-                step = step - 1;
-            }
+    $scope.refresh = function () {
+
+        var selectArray = [];
+
+        $.each($('select'), function () {
+            selectArray.push(this);
+        });
+
+        $.each((selectArray), function (key, item) {
+            $scope.load($(item).val(), item)
         });
     }
-    
-    $scope.$watch('step', function(){
-        console.log('changed');
+
+    $scope.$watch('step', function () {
+        $scope.refresh();
     });
+
+    $scope.invert = function () {
+        $('.image-container').toggleClass('invert');
+    }
+
+    $scope.step = 1;
 }]);
