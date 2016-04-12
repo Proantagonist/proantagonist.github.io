8d783ca5bfa932bc66d5e12aa7ad1c026ed35e3c
Proantagonist <imacleopard2010@gmail.com>
2016-04-12 00:58:39 -0500

    Added detection for missing fields

--- Projects/YelpScraperJS/scraper.js
+++ Projects/YelpScraperJS/scraper.js
@@ -2,6 +2,7 @@ var jsonString = [];
 var stringData;
 var startingPage = document.querySelectorAll(".pagination-links .current")[0].children[0].innerHTML;
 var currentPage = startingPage;
+var shouldGeocode;
 
 //getBusinesses();
 initialize();
@@ -61,9 +62,26 @@ function getPropertiesForBusinesses(data) {
     $.each(data[0], function (key, val) {
         var bizName = $(val).find('.biz-name span').text();
         var bizFullAddress = $(val).find('.secondary-attributes address').html();
-        var bizRating = $(val).find('.rating-large i').attr('title').split(' ')[0];
-        var bizReviewCount = $(val).find('.review-count').text().trim().split(" ")[0]
-        var bizCost = $(val).find('.price-range').text().length;
+
+        if ($(val).find('.rating-large i').attr('title') !== undefined) {
+            //Check if there are any reviews for this business
+            var bizRating = $(val).find('.rating-large i').attr('title').split(' ')[0];
+        } else {
+            var bizRating = null;
+        }
+
+        if ($(val).find('.review-count').text() !== "") {
+            //Check to see if there is a review count for this business.
+            var bizReviewCount = $(val).find('.review-count').text().trim().split(" ")[0]
+        } else {
+            var bizReviewCount = null;
+        }
+
+        if ($(val).find('.price-range').text().length !== undefined) {
+            var bizCost = $(val).find('.price-range').text().length;
+        } else {
+            var bizCost = null;
+        }
         var bizType = $(val).find('.category-str-list').text().replace(/\s\s+/g, ' ').trim();
 
         //==================================================================================
@@ -71,43 +89,61 @@ function getPropertiesForBusinesses(data) {
         var bizPopularity; // Some criteria we will have to develop to set a business radius
         //==================================================================================
         //==================================================================================
+        debugger;
+        var trimmedSplitAddress = $.trim(bizFullAddress).split(',');
 
-        var streetAddress = $.trim(bizFullAddress).split('<br>')[0];
-        var city = $.trim(bizFullAddress).split('<br>')[1].split(',')[0];
-        var state = $.trim(bizFullAddress).split('<br>')[1].split(',')[1].trim().split(" ")[0];
-        var zip = $.trim(bizFullAddress).split('<br>')[1].split(',')[1].trim().split(" ")[1];
+        if (trimmedSplitAddress[0].indexOf('<') === -1) {
+            var streetAddress = 'null'
+            var city = trimmedSplitAddress[0];
+            var state = trimmedSplitAddress[1].trim().split(' ')[0];
+            var zip = trimmedSplitAddress[1].trim().split(' ')[1];
+            shouldGeocode = false;
+        } else {
+            //            var streetAddress = $.trim(bizFullAddress).split('<br>')[0];
+            var streetAddress = trimmedSplitAddress[0].split('<br>')[0];
+            //            var city = $.trim(bizFullAddress).split('<br>')[1].split(',')[0];
+            var city = trimmedSplitAddress[0].split('<br>')[1]
+                //            var state = $.trim(bizFullAddress).split('<br>')[1].split(',')[1].trim().split(" ")[0];
+            var state = trimmedSplitAddress[1].trim().split(' ')[0];
+            //            var zip = $.trim(bizFullAddress).split('<br>')[1].split(',')[1].trim().split(" ")[1];
+            var zip = trimmedSplitAddress[1].trim().split(' ')[1];
+            shouldGeocode = true;
+        }
 
-        //-------------------GEOCODE
+        //-------------------BEGIN GEOCODE
 
-        var newPathGeocode = "https://geoservices.tamu.edu/Services/Geocode/WebService/GeocoderWebServiceHttpNonParsed_V04_01.aspx?streetAddress=" + streetAddress.replace(/ /g, "%20") + "&city=" + city.replace(/ /g, "%20") + "&state=" + state.replace(/ /g, "%20") + "&zip=" + zip.replace(/ /g, "%20") + "&apikey=demo&format=json&census=true&censusYear=2000|2010&notStore=false&version=4.01"
+        if (shouldGeocode === true) {
+            var newPathGeocode = "https://geoservices.tamu.edu/Services/Geocode/WebService/GeocoderWebServiceHttpNonParsed_V04_01.aspx?streetAddress=" + streetAddress.replace(/ /g, "%20") + "&city=" + city.replace(/ /g, "%20") + "&state=" + state.replace(/ /g, "%20") + "&zip=" + zip.replace(/ /g, "%20") + "&apikey=demo&format=json&census=true&censusYear=2000|2010&notStore=false&version=4.01"
 
-//
-//        var newPathGeocode = "https://geoservices.tamu.edu/Services/Geocode/WebService/GeocoderWebServiceHttpNonParsed_V04_01.aspx?streetAddress=" + streetAddress.replace(/ /g, "%20") + "&city=" + city.replace(/ /g, "%20") + "&state=" + state.replace(/ /g, "%20") + "&zip=" + zip.replace(/ /g, "%20") + "&apikey=f4db442a16d44ab7b9acbff707340245&format=json&census=true&censusYear=2000|2010&notStore=false&version=4.01"
+            //>>PERSONAL API
+            //        var newPathGeocode = "https://geoservices.tamu.edu/Services/Geocode/WebService/GeocoderWebServiceHttpNonParsed_V04_01.aspx?streetAddress=" + streetAddress.replace(/ /g, "%20") + "&city=" + city.replace(/ /g, "%20") + "&state=" + state.replace(/ /g, "%20") + "&zip=" + zip.replace(/ /g, "%20") + "&apikey=f4db442a16d44ab7b9acbff707340245&format=json&census=true&censusYear=2000|2010&notStore=false&version=4.01"
 
-        $.ajax({
-            url: newPathGeocode
-            , type: "GET"
-            , dataType: "json"
-            , success: function (data, status, jqXHR) {
-                var geocodeObject = data.OutputGeocodes[0].OutputGeocode;
+            $.ajax({
+                url: newPathGeocode
+                , type: "GET"
+                , dataType: "json"
+                , success: function (data, status, jqXHR) {
+                    var geocodeObject = data.OutputGeocodes[0].OutputGeocode;
 
-                var lat = geocodeObject.Latitude;
-                var long = geocodeObject.Longitude;
+                    var lat = geocodeObject.Latitude;
+                    var lon = geocodeObject.Longitude;
 
-                addToJSONObject(bizName, streetAddress, city, state, zip, bizRating, bizReviewCount, bizCost, bizType, lat, long);
+                    addToJSONObject(bizName, streetAddress, city, state, zip, bizRating, bizReviewCount, bizCost, bizType, lat, lon);
 
-            }
-        });
+                }
+            });
+        } else if (shouldGeocode === false) {
+            var lat = null;
+            var lon = null;
+            addToJSONObject(bizName, streetAddress, city, state, zip, bizRating, bizReviewCount, bizCost, bizType, lat, lon);
+        }
 
         //-------------------END GEOCODE
 
-        //        addToJSONObject(bizName, streetAddress, city, state, zip, bizRating, bizReviewCount, bizCost, bizType);
     })
-
-    //    stringData = JSON.stringify(jsonString);
 }
 
-function addToJSONObject(name, streetAddress, city, state, zip, rating, reviews, cost, type, lat, long) {
+function addToJSONObject(name, streetAddress, city, state, zip, rating, reviews, cost, type, lat, lon) {
     jsonString.push({
         name: name
         , streetAddress: streetAddress
@@ -119,7 +155,7 @@ function addToJSONObject(name, streetAddress, city, state, zip, rating, reviews,
         , price: cost
         , type: type
         , lat: lat
-        , long: long
+        , lon: lon
     });
 
     stringData = JSON.stringify(jsonString);
